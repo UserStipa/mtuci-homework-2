14f0ac862f93c62f7aefdf741a4cdb2a47a570b3
Dmitry Kovylyaev <dkovylyaev@bug-busters.ru>
2025-05-17 18:01:50 +0300

    Homework - 1

---
+++ src/main/java/org/example/ShellSort.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class ShellSort {
+
+    public static void run(int[] array) {
+        int n = array.length;
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int key = array[i];
+                int j = i;
+                while (j >= gap && array[j - gap] > key) {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+                array[j] = key;
+            }
+        }
+    }
+}
---
+++ src/test/java/SortsTest.java
@@ -0,0 +1,49 @@
+import org.example.QuickSort;
+import org.example.ShellSort;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SortsTest {
+
+    private static final int length = 1000000;
+    private static final int[] array = new int[length];
+
+    @BeforeAll
+    public static void setUp() {
+        for (int i = 0; i < length; i++) {
+            array[i] = new Random().nextInt();
+        }
+        System.out.println("Length of array: " + array.length);
+    }
+
+
+    @Test
+    public void shellSort() {
+        int[] arr = array.clone();
+        long startTime = System.currentTimeMillis();
+        ShellSort.run(arr);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Test: Shell sort: " + (endTime - startTime) + " milliseconds");
+    }
+
+    @Test
+    public void quickSort() {
+        int[] arr = array.clone();
+        long startTime = System.currentTimeMillis();
+        QuickSort.run(arr, 0, arr.length - 1);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Test: Quick sort: " + (endTime - startTime) + " milliseconds");
+    }
+
+    @Test
+    public void javaSort() {
+        int[] arr = array.clone();
+        long startTime = System.currentTimeMillis();
+        Arrays.sort(arr);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Test: Arrays sort (Java): " + (endTime - startTime) + " milliseconds");
+    }
+}
